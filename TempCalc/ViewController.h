23371ab3e0faafd08c547e131e1fc3265c8083d2
Hemant Torsekar <hvt@me.com>
2015-04-14 02:33:40 -0400

    Storyboard and outlets set up for Temperature Converter.

--- TempCalc/ViewController.h
+++ TempCalc/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UITextField *tempText;
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
 
 @end
 
