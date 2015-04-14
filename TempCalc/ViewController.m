4859b36718b2187c5290fbe0a39d2cff08616edf
Hemant Torsekar <hvt@me.com>
2015-04-14 02:14:56 -0400

    Initial Commit

---
+++ TempCalc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TempCalc
+//
+//  Created by Hemant V. Torsekar on 2015-04-14.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
